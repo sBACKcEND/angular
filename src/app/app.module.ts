c23fc70574d75fd76968bc270ba6450aed09fc9f
Sharas <sharas111@yahoo.com>
2022-10-20 00:19:35 +0300

    first task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { RezervuarasComponent } from './components/rezervuaras/rezervuaras.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { NewTaskComponent } from './components/new-task/new-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RezervuarasComponent,
+    TaskListComponent,
+    NewTaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
